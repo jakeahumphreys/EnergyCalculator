1ae83a3e6bab37ae2ace14fbaeda4522cea8bca7
jakeahumphreys <jakehumphreys@outlook.com>
2022-05-04 14:02:58 +0100

    Removed unnessecary dependencies.

--- Types/Appliance.cs
+++ Types/Appliance.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EnergyCalculator.Types
+﻿namespace EnergyCalculator.Types
 {
     public class Appliance
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
         public decimal StandbyWatts { get; set; }
-        public decimal UsageWatts { get; set; }
+        public decimal UsageWatts { get; set; }      
     }
 }
